3ec5b1dbf2a83a1f17229d02c5a6da592f7cfde0
VanAn19 <mrfeed69@gmail.com>
2024-11-01 23:14:47 +0700

    1/11

--- client/app/(root)/profile/page.tsx
+++ client/app/(root)/profile/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react';
 import { Form, Button, Input, message, Upload, Slider, Modal } from 'antd';
 import { UploadOutlined } from '@ant-design/icons';
 import Image from 'next/image';
